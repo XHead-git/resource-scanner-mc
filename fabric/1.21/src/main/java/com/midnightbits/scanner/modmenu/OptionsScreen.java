02783234bfaa7c618da1fb047f6e76ae91200b24
XHead-git <38805138+XHead-git@users.noreply.github.com>
2025-02-27 15:01:41 -0100

    Update OptionsScreen.java

--- fabric/1.21/src/main/java/com/midnightbits/scanner/modmenu/OptionsScreen.java
+++ fabric/1.21/src/main/java/com/midnightbits/scanner/modmenu/OptionsScreen.java
@@ -87,8 +87,8 @@ public class OptionsScreen extends SingleColumnOptions {
     static Function<Integer, Text> makeShowMessageLabeler() {
         return (showmessage) -> {
             if (showmessage == 0)
-                return translatableOption("Disabled", 0);
-            return translatableOption("Enabled", 1);
+                return translatableOption("showmessage/disabled", 0);
+            return translatableOption("showmessage/enabled", 1);
         };
     }
 
